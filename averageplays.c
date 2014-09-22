1fa85b1fc16aecd5fc70247f8ba5df82797a8343
Natriceus <natriceus@gmail.com>
2014-09-23 00:06:50 +0200

    Changed initialisations.

--- averageplays.c
+++ averageplays.c
@@ -4,7 +4,6 @@
 
 int main(int argc, char **argv){
 
-    double argument, days;
     time_t current_time;
     time(&current_time);
     struct tm then = *localtime(&current_time);
@@ -12,11 +11,11 @@ int main(int argc, char **argv){
     then.tm_mon = 9;
     then.tm_mday = 16;
 
-    days = (difftime(current_time, mktime(&then)))/86400;
+    double days = (difftime(current_time, mktime(&then)))/86400;
     printf("%.f days have passed.\n", days);
 
     if(argv[1] != NULL){
-        argument = atof(argv[1]);
+        double argument = atof(argv[1]);
         printf("%.3f plays per average day.\n", argument/days);
     }
 
